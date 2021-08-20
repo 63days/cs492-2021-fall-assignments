368b95257208357a73ca43bc2b54c20499403897
Juil Koo <63days@kaist.ac.kr>
2021-08-20 10:52:37 +0900

    Create install_essential.sh

---
+++ install_essential.sh
@@ -0,0 +1,12 @@
+#dependencies
+sudo apt-get install -y mesa-utils \
+	xorg-dev \
+	freeglut3 \
+	freeglut3-dev \
+	mesa-common-dev \
+	libosmesa6-dev \
+	libgl1-mesa-dev \
+	libglu1-mesa-dev \
+	libx11-dev \
+	libxi-dev \
+	libxmu-dev
