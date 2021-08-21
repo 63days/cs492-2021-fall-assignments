4e3408dd4583691679c07ebddf4715f2b6c7f122
Juil Koo <63days@kaist.ac.kr>
2021-08-21 21:25:29 +0900

    Update install_essential.sh

--- install_essential.sh
+++ install_essential.sh
@@ -1,16 +1,20 @@
 #dependencies
-sudo apt-get install -y mesa-utils \
-	cmake \
-	xorg-dev \
-	freeglut3 \
-	freeglut3-dev \
-	mesa-common-dev \
-	libosmesa6-dev \
-	libgl1-mesa-dev \
-	libglu1-mesa-dev \
-	libx11-dev \
-	libxi-dev \
-	libxmu-dev \
-	x11-apps \
-	libeigen3-dev
-# If using Xquartz in MAC, defaults write org.macosforge.xquartz.X11 enable_iglx -bool true and re-run Xquartz may need to set xhost + IP
+sudo apt-get update && sudo apt-get install -y \
+        build-essential \
+        tmux \
+        apt-utils \
+        mesa-utils \
+        cmake \
+        xorg-dev \
+        freeglut3 \
+        freeglut3-dev \
+        mesa-common-dev \
+        libosmesa6-dev \
+        libgl1-mesa-dev \
+        libglu1-mesa-dev \
+        libx11-dev \
+        libxi-dev \
+        libxmu-dev \
+        x11-apps \
+        libeigen3-dev
+# If using Xquartz in MAC, you should run "defaults write org.macosforge.xquartz.X11 enable_iglx -bool true" and re-run Xquartz. May need to set xhost + IP
